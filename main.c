d3cae1d9c92ff1431ecb6a7d06bd314f76af8826
praphavanh <55821864+praphavanh@users.noreply.github.com>
2019-09-25 22:32:20 -0500

    Update main.c
    Print Hello Holberton 32 times instead of Holberton 10 times

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
+/* Print out "Hello Holberton" 32 times from original "Holberton" 10 times */
+
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  /* 10 to 32 */
+  for (i = 0; i < 32; i++)
   {
-    printf("Holberton\n");
+    /* Hello has been added */
+    printf("Hello Holberton\n");
   }
   return 0;
 }
